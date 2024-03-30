cf4e28c27b3cae3333500d3c263271eeda406807
ANSHUL <you@example.com>
2024-03-31 01:05:12 +0530

    changes

---
+++ src/Auth/Login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Login = ()=>{
+    return(
+        <>
+        <label htmlFor="">Email</label>
+        <br></br>
+        <input type="text" placeholder="email" />
+        <br></br>
+        <label htmlFor="">Password</label>
+        <br></br>
+        <input type="text" placeholder="password" />
+    <br></br>
+        <button>Login</button>
+        
+        </>
+    )
+    
+  
+}
+export default Login
