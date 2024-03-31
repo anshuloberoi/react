4c3241a8536e07341431f1e271c0690dccfdceee
ANSHUL <you@example.com>
2024-04-01 00:26:36 +0530

    changes

--- src/Crud/Add.jsx
+++ src/Crud/Add.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import List from "./List";
 
 const Add =()=>{
+const [form,setform]=useState({
+email:'',
+password:''
+})
+
+const onsubmit =(event) =>{
+    event.preventDefault()
+}
+
+const eventForm =(e)=>{
+    console.log(e);
+}
  return(
     <>
     
-    <p>Hello Add</p>
+    <h1>Form</h1>
+<form action="" onSubmit={onsubmit}>
+<label>email</label>
+<input placeholder="email" value={form.email}  onChange={ (e)=> eventForm()} />
 
+<label>Password</label>
+<input placeholder="password" value={form.password} onChange={ (e)=> eventForm()} />
+
+<button type="submit">Sign Up</button>
+</form>
     <List></List>
     </>
  )   
